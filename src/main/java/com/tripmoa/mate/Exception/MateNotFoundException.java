76d6c777f34524662b3240964c61ba52ceec13c7
Haejin Kim <kkkh_1007@naver.com>
2026-04-18 11:10:37 +0900

    refactor(mate, chat): move mate and chat packages out of community
    - com.tripmoa.community.mate → com.tripmoa.mate
    - com.tripmoa.community.chat → com.tripmoa.chat
    - 전체 import 경로 일괄 수정

---
+++ src/main/java/com/tripmoa/mate/Exception/MateNotFoundException.java
@@ -0,0 +1,19 @@
+package com.tripmoa.mate.Exception;
+
+import com.tripmoa.global.exception.BusinessException;
+import com.tripmoa.global.exception.ErrorCode;
+
+public class MateNotFoundException extends BusinessException {
+
+    public MateNotFoundException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public static MateNotFoundException postNotFound() {
+        return new MateNotFoundException(ErrorCode.POST_NOT_FOUND);
+    }
+
+    public static MateNotFoundException applicationNotFound() {
+        return new MateNotFoundException(ErrorCode.APPLICATION_NOT_FOUND);
+    }
+}
---
+++ src/main/java/com/tripmoa/mate/domain/MateDomain.java
@@ -0,0 +1,64 @@
+package com.tripmoa.mate.domain;
+
+import com.tripmoa.mate.Exception.InvalidBudgetException;
+import com.tripmoa.mate.Exception.InvalidParticipantInfoException;
+import com.tripmoa.mate.Exception.InvalidScheduleException;
+import com.tripmoa.mate.Exception.ParticipantFullException;
+import com.tripmoa.user.entity.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class MateDomain {
+
+    public void validateCreate(MatePost post) {
+        validateSchedule(post.getStartDate(), post.getEndDate());
+        validateParticipantInfo(post.getCurrentParticipant(), post.getMaxParticipant());
+        validateBudget(post.getBudget());
+    }
+
+    private void validateSchedule(LocalDate startDate, LocalDate endDate) {
+        if(endDate.isBefore(startDate)) {
+            throw InvalidScheduleException.endDateBeforeStart();
+        }
+        if(startDate.isBefore(LocalDate.now())) {
+            throw InvalidScheduleException.startDateBeforeNow();
+        }
+    }
+
+    private void validateParticipantInfo(Integer current, Integer max) {
+        if(current < 1) {
+            throw InvalidParticipantInfoException.currentTooLow();
+        }
+        if(max < 2) {
+            throw InvalidParticipantInfoException.maxTooLow();
+        }
+        if(current > max) {
+            throw InvalidParticipantInfoException.currentExceedsMax();
+        }
+        if(current.equals(max)) {
+            throw InvalidParticipantInfoException.alreadyFullyBooked();
+        }
+    }
+
+    private void validateBudget(Integer budget) {
+        if(budget < 0) {
+            throw new InvalidBudgetException();
+        }
+    }
+
+    public boolean isFullyBooked(MatePost post) {
+        return post.getCurrentParticipant()>=post.getMaxParticipant();
+    }
+
+    public void validateApply(MatePost post) {
+        if(isFullyBooked(post)) {
+            throw new ParticipantFullException();
+        }
+    }
+
+    public boolean isAuthor(MatePost post, User user) {
+        return post.getUser().getId().equals(user.getId());
+    }
+}
